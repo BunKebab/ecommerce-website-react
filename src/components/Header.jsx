af38649c413702518fb114fb875e342f35f4b45a
Sushi <shaheer.anser@gmail.com>
2024-06-26 17:05:49 +0500

    initial header component completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react"
+import {Link} from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <div className="container">
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
+            <Link to='/' className="mr-5 hover:text-gray-900">Home</Link>
+            <Link to='/products' className="mr-5 hover:text-gray-900">Products</Link>
+            <Link to='/cart' className="mr-5 hover:text-gray-900">Cart</Link>
+            <Link className="hover:text-gray-900">Fourth Link</Link>
+          </nav>
+          <Link to='/' className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+            <span className="ml-3 text-xl">Ecommerce</span>
+          </Link>
+          <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0">
+            <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+              <Link to='/login'>
+                Login
+              </Link>
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-4 h-4 ml-1"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
